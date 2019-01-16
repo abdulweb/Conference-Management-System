d65befdbf61964841f4f6fb354a0b9884e9515e4
Olami001 <abdulrasheeda9@gmail.com>
2019-01-16 22:00:32 +0100

    header logout set

--- admin/includes/topbar.php
+++ admin/includes/topbar.php
@@ -167,7 +167,7 @@
                                     <li ><a href="profile.php"><i class="ti-user m-r-5"></i> Profile</a></li>
                                     <li><a href="javascript:void(0)"><i class="ti-settings m-r-5"></i> Settings</a></li>
                                     <li><a href="javascript:void(0)"><i class="ti-lock m-r-5"></i> Lock screen</a></li>
-                                    <li><a href="javascript:void(0)"><i class="ti-power-off m-r-5"></i> Logout</a></li>
+                                    <li><a href="../index.php"><i class="ti-power-off m-r-5"></i> Logout</a></li>
                                 </ul>
                             </li>
 
